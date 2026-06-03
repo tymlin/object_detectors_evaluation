4c2185eee12e60f34569e0c3e2ceaeebd90ce785
Tymoteusz Lindner <tymoteusz.lindner@gmail.com>
2026-06-03 19:49:26 +0200

    updated download dataset scripts

--- scripts/dataset/download_coco_dataset.py
+++ scripts/dataset/download_coco_dataset.py
@@ -1,15 +1,21 @@
 from object_detectors_evaluation.consts import FIFTYONE_DATASETS_DIRPATH
 from object_detectors_evaluation.datasets.fiftyone import download_coco_dataset
 from object_detectors_evaluation.loggers import logger
+from object_detectors_evaluation.utils.types import Split
 
+SPLIT: Split = "validation"
+CLASSES_OF_INTEREST = ["person"]
 MAX_SAMPLES: int | None = 5
 
 
 def main() -> None:
-    logger.info(f"Downloading tiny dataset `COCO` to path: '{FIFTYONE_DATASETS_DIRPATH}'")
+    logger.info(
+        f"Downloading dataset `COCO` (split: {SPLIT}, #samples: {MAX_SAMPLES}) "
+        f"to path: '{FIFTYONE_DATASETS_DIRPATH}'"
+    )
     coco_dataset, coco_path = download_coco_dataset(
-        split="validation",
-        classes=["person"],
+        split=SPLIT,
+        classes=CLASSES_OF_INTEREST,
         max_samples=MAX_SAMPLES,
     )
     logger.info(f"Dataset `COCO` downloaded: {coco_dataset.num_samples} samples at path: '{coco_path}'")
--- scripts/dataset/download_open_images_dataset.py
+++ scripts/dataset/download_open_images_dataset.py
@@ -1,15 +1,22 @@
 from object_detectors_evaluation.consts import FIFTYONE_DATASETS_DIRPATH
 from object_detectors_evaluation.datasets.fiftyone import download_open_images_dataset
 from object_detectors_evaluation.loggers import logger
+from object_detectors_evaluation.utils.types import Split
 
+SPLIT: Split = "test"
+CLASSES_OF_INTEREST = ["Person"]
+# CLASSES_OF_INTEREST = ["Person", "Human body", "Man", "Woman"]
 MAX_SAMPLES: int | None = 5
 
 
 def main() -> None:
-    logger.info(f"Downloading tiny dataset `Open Images` to path: '{FIFTYONE_DATASETS_DIRPATH}'")
+    logger.info(
+        f"Downloading dataset `Open Images` (split: {SPLIT}, #samples: {MAX_SAMPLES}) "
+        f"to path: '{FIFTYONE_DATASETS_DIRPATH}'"
+    )
     open_images_dataset, open_images_path = download_open_images_dataset(
-        split="test",
-        classes=["Person"],
+        split=SPLIT,
+        classes=CLASSES_OF_INTEREST,
         max_samples=MAX_SAMPLES,
     )
     logger.info(
